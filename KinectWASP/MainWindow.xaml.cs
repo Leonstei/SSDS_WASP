5583ef65959e791dc0b07cdc9970b63dcb713116
Leon Steinbach <leon.steinbach@smail.th-koeln.de>
2025-02-03 22:10:30 +0100

    initial commit

---
+++ KinectWASP/MainWindow.xaml.cs
@@ -0,0 +1,134 @@
+﻿using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using Microsoft.Kinect;
+
+namespace KinectWASP
+{
+    public partial class MainWindow : Window
+    {
+        private KinectSensor _kinectSensor;
+        
+        //Intermediate storage for the depth data received from the camera 
+        private DepthImagePixel[] _depthPixels;
+        
+        //Bitmap that will hold depth information
+        private BitmapSource _depthBitmap;
+        
+        private WriteableBitmap _colorBitmap;
+        
+        //storage for color data
+        private byte[] _clolorData;
+        
+        //storage for depth data
+        private byte[] _depthData;
+        
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += MainWindow_Loaded;
+            Closing += MainWindow_Closing;
+            MouseDown += Button_Click;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            _kinectSensor = KinectSensor.KinectSensors.FirstOrDefault(x => x.Status == KinectStatus.Connected);
+            if (_kinectSensor != null)
+            {
+                _kinectSensor.DepthStream.Enable(DepthImageFormat.Resolution640x480Fps30);
+                _kinectSensor.ColorStream.Enable(ColorImageFormat.RgbResolution640x480Fps30);
+                _kinectSensor.ColorFrameReady += KinectSensor_ColorFrameReady;
+                _kinectSensor.DepthFrameReady += KinectSensor_DepthFrameReady;
+                
+                _depthPixels = new DepthImagePixel[_kinectSensor.DepthStream.FramePixelDataLength];
+
+                _depthData = new byte[_kinectSensor.DepthStream.FramePixelDataLength * sizeof(int)]; 
+                
+                _depthBitmap = BitmapSource.Create(
+                    _kinectSensor.DepthStream.FrameWidth, _kinectSensor.DepthStream.FrameHeight, 96.0, 96.0, 
+                    PixelFormats.Gray8, null, _depthData, _kinectSensor.DepthStream.FrameWidth );
+                DepthVideo.Source = _depthBitmap;
+
+                _clolorData = new byte[_kinectSensor.ColorStream.FramePixelDataLength];
+                _colorBitmap = new WriteableBitmap(640, 480, 96.0, 96.0, System.Windows.Media.PixelFormats.Bgr32, null);
+                KinectVideo.Source = _colorBitmap;
+
+                _kinectSensor.Start();
+            }
+        }
+
+        private void KinectSensor_DepthFrameReady(object sender, DepthImageFrameReadyEventArgs e)
+        {
+            using (DepthImageFrame depthFrame = e.OpenDepthImageFrame())
+            {
+                if (depthFrame != null)
+                {
+                    // Copy the pixel data from the image to a temporary array
+                    depthFrame.CopyDepthImagePixelDataTo(_depthPixels);
+                    
+                    // Get the min and max reliable depth for the current frame
+                    int minDepth = depthFrame.MinDepth;
+                    int maxDepth = depthFrame.MaxDepth;
+                    
+                    for (int i = 0; i < _depthPixels.Length; ++i)
+                    {
+                        // Get the depth for this pixel
+                        short depth = _depthPixels[i].Depth;
+                        
+                        if (depth >= minDepth && depth <= maxDepth)
+                        {
+                            byte brightness = (byte)(255 - ((depth - minDepth) * 255 / (maxDepth - minDepth)));
+                            _depthData[i] = brightness; // Helle Objekte = Nah / Dunkle Objekte = Fern
+                            
+                        }
+                        else
+                        {
+                            // Setzen Sie außerhalb des Bereichs liegende Pixel auf Schwarz (ARGB = 0)
+                            _depthData[i] = 0;
+                        }
+                        
+                    }
+                    BitmapSource grayscaleBitmap = BitmapSource.Create(
+                        depthFrame.Width,
+                        depthFrame.Height,
+                        96,
+                        96,
+                        PixelFormats.Gray8,
+                        null,
+                        _depthData,
+                        depthFrame.Width
+                    );
+                    DepthVideo.Source  = grayscaleBitmap;
+                }
+                
+            }
+        }
+
+        private void KinectSensor_ColorFrameReady(object sender, ColorImageFrameReadyEventArgs e)
+        {
+            using (var frame = e.OpenColorImageFrame())
+            {
+                if (frame != null)
+                {
+                    frame.CopyPixelDataTo(_clolorData);
+                    _colorBitmap.WritePixels(new Int32Rect(0, 0, frame.Width, frame.Height), _clolorData, frame.Width * 4, 0);
+                }
+            }
+        }
+
+        private void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (_kinectSensor != null && _kinectSensor.IsRunning)
+            {
+                _kinectSensor.Stop();
+            }
+        }
+    }
+}
